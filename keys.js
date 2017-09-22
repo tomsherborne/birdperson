1dabe7d336f471361f9e65231d6f57e6f64534f2
muhts <zceemmu@ucl.ac.uk>
2017-09-22 17:14:54 +0100

    first commit

--- keys.js
+++ keys.js
@@ -1,7 +1,8 @@
 // Enter the API keys for each API here
 WATSONKEY = 'AplGchWYz9g_T-hMr7sVFhQr-wxcLdJoMzeDF96Ju2IQ';
+WATSONURL = "https://gateway.watsonplatform.net/tone-analyzer/api";
 WATSONUSER = '60db72b0-38ed-4e28-bd34-4453f6907220';
 WATSONPW = 'GIyYdgz8omao';
 TWITTERUSER = '';
 TWITTERPASSWORD = '';
-module.exports = {WATSONKEY,TWITTERUSER,TWITTERPASSWORD};
+module.exports = {WATSONKEY, WATSONUSER, WATSONPW,TWITTERUSER,TWITTERPASSWORD};

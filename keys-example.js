3d80c2b0e3f2c8dc67f3a2f45e452ad01673db61
trsherborne <tr.sherborne@gmail.com>
2017-09-23 20:51:26 +0100

    Add example of keys for future open sourcing

---
+++ keys-example.js
@@ -0,0 +1,19 @@
+// Enter the API keys for each API here
+WATSONURL = "https://gateway.watsonplatform.net/tone-analyzer/api";
+WATSONKEY = '';
+WATSONUSER = '';
+WATSONPW = '';
+TWITCONSUMERKEY = '';
+TWITCONSUMERSECRET = '';
+TWITACCESSTOKEN = '';
+TWITACCESSTOKENSECRET = '';
+module.exports = {
+  WATSONURL,
+  WATSONKEY,
+  WATSONUSER,
+  WATSONPW,
+  TWITCONSUMERKEY,
+  TWITCONSUMERSECRET,
+  TWITACCESSTOKEN,
+  TWITACCESSTOKENSECRET
+};
